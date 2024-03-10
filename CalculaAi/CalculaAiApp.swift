a29638f4e98379aca586711f133340962749df68
Jean Camargo <jeancamargoreal@icloud.com>
2024-03-10 19:31:53 -0300

    Initial Commit

---
+++ CalculaAi/CalculaAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculaAiApp.swift
+//  CalculaAi
+//
+//  Created by Jean Camargo on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculaAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
